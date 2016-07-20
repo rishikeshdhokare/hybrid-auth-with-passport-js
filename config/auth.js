86ca82f618e79786889c214b8a1e035d0629005b
Rishikesh Dhokare <rdhokare@equalexperts.com>
2016-07-20 12:35:04 +0530

    Initial commit.

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID'      : 'your-client-id',
+        'clientSecret'  : 'your-client-secret',
+        'callbackURL'   : 'http://localhost:5000/auth/facebook/callback'
+    },
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:5000/auth/twitter/callback'
+    },
+    'googleAuth' : {
+        'clientID'      : 'your-client-id',
+        'clientSecret'  : 'your-client-secret',
+        'callbackURL'   : 'http://localhost:5000/auth/google/callback'
+    }
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+var port = process.env.PORT || 5000;
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+require('./config/passport.js')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.set('view engine', 'ejs');
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
